42134afe6a0a7c8dc6486f6e03d115c2951a4ce1
Daniel89987 <d_ming@u.pacific.edu>
2024-09-12 12:31:32 -0700

    Basic multithreading

--- nodes/scripts/level_manager.gd
+++ nodes/scripts/level_manager.gd
@@ -9,6 +9,8 @@ static var _cur_level: Level = null
 signal level_set
 signal checkpoint_set
 
+var next_level
+
 # Sets current level
 func set_cur_level(level: Level) -> void:
 	_cur_level = level
@@ -26,12 +28,21 @@ func _ready() -> void:
 	GameManager.current_level_manager = self
 	#$"/root/LevelManager".connect("complete_level", next)
 	#$GoalTile.connect("complete_level", next)
+	#var preloader = $resource_loader  # Adjust the path to your Preloader node
+	#preloader.connect("finished_loading", self, "_on_preloading_done")
+
+func level_loaded(instance1):
+	#add_child(instance1)
+	next_level = instance1
+
 
 #func _on_goal_tile_complete_level() -> void:
 func next() -> void:
-	print("Switching level")
+	print("Switching level", next_level)
 	print(SpawnPoint.check_point_level)
-	get_tree().call_deferred("change_scene_to_file", "res://nodes/scenes/level.tscn")
+	#get_tree().call_deferred("change_scene_to_file", "res://nodes/scenes/level.tscn")
+	#var scene = $instance
+	get_tree().call_deferred("change_scene_to_file",next_level)
 
 func on_death(body) -> void:
 	body.visible = false
---
+++ nodes/scripts/resource_scripts/resource_loader.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var thread: Thread
+
+signal finished_loading
+
+func _ready():
+	thread = Thread.new()
+	thread.start(preload_scenes)
+
+
+func preload_scenes():
+	# Preloading levels
+	#var scene1 = ResourceLoader.load("res://nodes/scenes/level.tscn")
+	var scene1 = load("res://nodes/scenes/level.tscn")
+	#var instance1 = scene1.instantiate()
+
+	# Add the instances to the scene tree if needed
+	# get_tree().root.add_child(instance1)
+	# get_tree().root.add_child(instance2)
+
+	# Signal that preloading is done
+	print("Scenes preloaded", "res://nodes/scenes/level.tscn")
+	emit_signal("finished_loading", "res://nodes/scenes/level.tscn")
+
+func _exit_tree():
+	thread.wait_to_finish()
