7ee5416c547f326c2aec848db1372cd68d9d5b0f
Daniel89987 <d_ming@u.pacific.edu>
2024-10-17 18:49:38 -0700

    Polished coin.gd, added timer for coin.tscn

--- coin.gd
+++ coin.gd
@@ -14,40 +14,55 @@ class_name Coin extends Tile
 const RESPAWN_OFFSET: Vector2 = Vector2(.5, 0)
 
 # Variables
+@export var total_coins: int
+
 var distances = []
 var cells = []
 
-func handle_collision(collider: Node) -> void:
-	if collider is Player:
-		for cell in get_used_cells():
-			var cell_world_position = Level.to_pixel_coords(Vector2(cell))
-			
-			#Change all the checkpoint tile without the signs into the tile that has the sign
-			if get_cell_atlas_coords(cell) == Vector2i(3, 1):
-				set_cell(cell, 1, Vector2i(4, 1))
-			
-			cells.append(cell)
-			print("distance to player: ",cell_world_position.distance_to(local_to_map(collider.global_position - Vector2(320, 180))))
-			#print("distance to player: ",cell_world_position.distance_to(collider.global_position - Vector2(640, 360)))
-			distances.append(cell_world_position.distance_to(Level.to_pixel_coords(local_to_map(collider.position))))
-		var index = distances.find(distances.min(), 0)
-		
-		#print("all cells ", cells)
-		#print("and their perspective distances to player", distances)
-		
-		#If the checkpoint has the sign on it, we switch the tile to the one without the sign
-		var offset: Vector2i = Vector2i.ZERO
-		#while true:
-		var pos: Vector2i = cells[index] + offset
-		var coords := get_cell_atlas_coords(pos)
-		print(coords)
-		if coords == Vector2i(4, 1) or coords == Vector2i(0, 0):
-			set_cell(pos, 1, Vector2i(3, 1))
-			#break
-		elif coords == Vector2i(4, 2):
-			offset.y -= 1
-		#SpawnPoint.global_vector = Level.to_pixel_coords((Vector2(cells[index]) + RESPAWN_OFFSET))
-		#print(get_cell_atlas_coords(cells[index]))
+var can_collide = true
 
-		distances = []
-		cells = []
+func handle_collision(collider: Node) -> void:
+	if can_collide:
+		if collider is Player:
+			for cell in get_used_cells():
+				var cell_world_position = Level.to_pixel_coords(Vector2(cell))
+				
+				#Change all the checkpoint tile without the signs into the tile that has the sign
+				if get_cell_atlas_coords(cell) == Vector2i(3, 1):
+					set_cell(cell, 1, Vector2i(4, 1))
+				
+				cells.append(cell)
+				print("distance to player: ",cell_world_position.distance_to(local_to_map(collider.global_position - Vector2(320, 180))))
+				#print("distance to player: ",cell_world_position.distance_to(collider.global_position - Vector2(640, 360)))
+				distances.append(cell_world_position.distance_to(Level.to_pixel_coords(local_to_map(collider.position))))
+			var index = distances.find(distances.min(), 0)
+			
+			print("all cells ", cells)
+			print("selected index: ", index)
+			print(distances)
+			set_cell(cells[index], -1)
+			#print("and their perspective distances to player", distances)
+			
+			#Recycled code from checkpoint, basically, if the player touches a coin, set the coin's atlas 
+			#coordinates to an invalid vector (5, 5), removing the coin from the screen
+			var offset: Vector2i = Vector2i.ZERO
+			#while true:
+			var pos: Vector2i = cells[index] + offset
+			var coords := get_cell_atlas_coords(pos)
+			#print(coords)
+			if coords == Vector2i(4, 1) or coords == Vector2i(0, 0):
+				set_cell(pos, 1, Vector2i(5, 5))
+				#break
+			elif coords == Vector2i(4, 2):
+				offset.y -= 1
+			total_coins -= 1
+
+			distances = []
+			cells = []
+			print(total_coins)
+			can_collide = false
+			$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	can_collide = true
