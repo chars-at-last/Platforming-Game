6c766d3f8af93cc30198e805ced5c74810e8d543
Daniel89987 <d_ming@u.pacific.edu>
2024-12-06 14:00:52 -0800

    Added information in the help screen

--- nodes/scripts/help.gd
+++ nodes/scripts/help.gd
@@ -7,6 +7,10 @@ func _ready() -> void:
 	var camera = CurrentCameraZoom.camera_zoom_level
 	print("current camera zoom level for help screen ", camera)
 	$GridContainer.get_parent().scale = Vector2(1 / camera.x, 1 / camera.y)
+	if camera.y < 0.8:
+		$GridContainer.get_parent().position += Vector2(0, 200)
+	if camera.y == 0.8:
+		$GridContainer.get_parent().position += Vector2(0, 100)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
